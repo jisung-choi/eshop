dc59edac10837a0d29ba4dc7112e21043e37ab66
James Jisung Choi <70044510+jisung-choi@users.noreply.github.com>
2023-04-19 12:36:05 -0700

    Inits, project structure, style

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { RouterModule } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
-import { appRoutes } from "./app.routes";
 import { NxWelcomeComponent } from "./nx-welcome.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from "@eshop/ui";
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path:'products', component:ProductListComponent}
+]
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: "enabledBlocking" }),
+    RouterModule.forRoot(routes),
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ libs/ui/src/lib/slider/slider.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-slider',
+  templateUrl: './slider.component.html',
+})
+export class SliderComponent {
+
+}
