da4808973b74ca1063e87c3be42d34f471f28bc9
James Jisung Choi <70044510+jisung-choi@users.noreply.github.com>
2023-04-21 15:59:11 -0700

    adds admin shell, sidebar, category list api call

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CategoriesService } from '@eshop/products';
+
+const UX_MODULE = [
+  CardModule,
+  ToolbarModule,
+  ButtonModule,
+  TableModule
+]
+
+const routes: Routes = [
+  { 
+    path: '', 
+    component: ShellComponent,
+    children: [
+      {
+        path:'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path:'categories',
+        component: CategoriesListComponent
+      },
+    ]  
+  },
+]
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    ...UX_MODULE
   ],
-  providers: [],
+  providers: [CategoriesService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService, Category } from '@eshop/products'
+
+@Component({
+  selector: 'admin-categories-list',
+  templateUrl: './categories-list.component.html',
+  styles: [
+  ]
+})
+export class CategoriesListComponent implements OnInit{
+  categories: Category[] = [];
+
+  constructor(private categoriesService: CategoriesService) {
+
+  }
+
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe(cats => {
+      this.categories = cats;
+    });
+  }
+}
+
